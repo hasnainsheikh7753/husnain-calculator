7de9e41bd67bb3a585b024a7bff3aeb0a62ad996
hasnain sheikh <husnainsheikh7753@gmail.com>
2023-10-13 03:50:49 +0500

    files ad

---
+++ app.js
@@ -0,0 +1,21 @@
+var display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function deleteLastChar() {
+    display.value = display.value.slice(0, -1);
+}
